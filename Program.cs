0b96a3b102d1b58b30dfa33e496f84e93d8387d4
shivam <shivam@DESKTOP-BT1O840>
2023-08-25 22:14:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public static class Logger
+{
+    public static void LogMessage(string message)
+    {
+        Console.WriteLine($"[{DateTime.Now}] {message}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger.LogMessage("This is a log message.");
+        Logger.LogMessage("Another log message.");
+
+        
+        Console.WriteLine("Program finished.");
+    }
+}
